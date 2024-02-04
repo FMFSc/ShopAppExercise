66bf112b23aaa6a411676384a8d8fd78592940a5
FMFSc Gandalf <fellipescirea@gmail.com>
2024-02-03 19:42:04 -0800

    Adding new items and changing logic for total calculation with value restriction

--- src/duke/choice/ShopAppExercise.java
+++ src/duke/choice/ShopAppExercise.java
@@ -17,7 +17,7 @@ public class ShopAppExercise {
         tax = 0.2;
         measurement = 3;
         total = 0;
-        
+
         //total = tax * price;
         System.out.println("Welcome to the Duke Choice Shop");
 
@@ -52,6 +52,8 @@ public class ShopAppExercise {
 
         Clothing item1 = new Clothing();
         Clothing item2 = new Clothing();
+        Clothing item3 = new Clothing();
+        Clothing item4 = new Clothing();
 
         item1.description = "Blue Jacket";
         item1.price = 20.9;
@@ -59,33 +61,40 @@ public class ShopAppExercise {
         item2.description = "Orange T-shirt";
         item2.price = 10.5;
         item2.size = "S";
-        
-        Clothing[] clothingList = new Clothing[2];
+        item3.description = "Green Scarf";
+        item3.price = 5;
+        item3.size = "S";
+        item4.description = "Blue T-shirt";
+        item4.price = 10.5;
+        item4.size = "S";
+
+        Clothing[] clothingList = new Clothing[4];
         clothingList[0] = item1;
         clothingList[1] = item2;
-        
+        clothingList[2] = item3;
+        clothingList[3] = item4;
+
         //just a test 2
-
- //       System.out.println("Item 1: " + item1.description + ", " + item1.price + ", " + item1.size);
- //       System.out.println("Item 2: " + item2.description + ", " + item2.price + ", " + item2.size);
-
-        
+        //       System.out.println("Item 1: " + item1.description + ", " + item1.price + ", " + item1.size);
+        //       System.out.println("Item 2: " + item2.description + ", " + item2.price + ", " + item2.size);
         //total = (item1.price + item2.price * 2) * (1 + tax);
-        
         //calculating total with a for each loop
-        for (Clothing item : clothingList){
-            System.out.println("Total = " + total);
-            total = total + item.price * (1 + tax);
+        for (Clothing item : clothingList)
+        {
+//            System.out.println("Total = " + total);
+            if (c1.size.equalsIgnoreCase(item.size) && total <= 15)
+            {
+                total = total + item.price * (1 + tax);
+            }
+
         }
-        
-        
+
         System.out.println("Total = " + total);
-        
-        for (Clothing item : clothingList){
-            System.out.println("Item: "+ item.description + ", " + item.price + ", " + item.size);
+
+        for (Clothing item : clothingList)
+        {
+            System.out.println("Item: " + item.description + ", " + item.price + ", " + item.size);
         }
-        
-        
 
     }
 
