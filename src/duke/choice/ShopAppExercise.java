140059872a444e95250f9424922c942473ad28f7
FMFSc Gandalf <fellipescirea@gmail.com>
2024-02-03 18:13:23 -0800

    Fix on switch element.

--- src/duke/choice/ShopAppExercise.java
+++ src/duke/choice/ShopAppExercise.java
@@ -5,7 +5,6 @@ package duke.choice;
  * @author fellipescirea
  */
 public class ShopAppExercise {
-    
 
     /**
      * @param args the command line arguments
@@ -14,58 +13,58 @@ public class ShopAppExercise {
         double tax;
         double total;
         int measurement;
-        
+
         tax = 0.2;
         measurement = 3;
-        
-        
-        
 
         //total = tax * price;
-        
-        
         System.out.println("Welcome to the Duke Choice Shop");
-        
+
         Customer c1 = new Customer();
-        
+
         c1.name = "Pinky";
-        
+
         System.out.println("Customer name is " + c1.name);
-        
+
         c1.size = "S";
-        
-        switch ( measurement ){
-            case 1, 2, 3:
-                System.out.println("Size: S");
+
+        switch (measurement)
+        {
+            case 1:
+            case 2:
+            case 3:
+                c1.size = "S";
                 break;
-            case 4, 5, 6:
-                System.out.println("Size: M");
+            case 4:
+            case 5:
+            case 6:
+                c1.size = "M";
                 break;
-            case 7, 8, 9:
-                System.out.println("Size: L");
+            case 7:
+            case 8:
+            case 9:
+                c1.size = "L";
                 break;
             default:
-                System.out.println("Size: X");
+                c1.size = "X";
         }
-        
-        
- //       Clothing item1 = new Clothing();
- //       Clothing item2 = new Clothing();
-        
- //       item1.description   = "Blue Jacket";
- //       item1.price         = 20.9;
- //       item1.size          = "M";
- //       item2.description   = "Orange T-shirt";
- //       item2.price         = 10.5;
- //       item2.size          = "S";
- //       
- //       System.out.println("Item 1: " + item1.description + ", "+item1.price +", " +item1.size);
- //       System.out.println("Item 2: " + item2.description + ", "+item2.price +", " +item2.size);
- //       
- //       
- //       total = (item1.price + item2.price*2)*(1+tax);
-//        System.out.println("Total = "+ total);
-        
+
+        Clothing item1 = new Clothing();
+        Clothing item2 = new Clothing();
+
+        item1.description = "Blue Jacket";
+        item1.price = 20.9;
+        item1.size = "M";
+        item2.description = "Orange T-shirt";
+        item2.price = 10.5;
+        item2.size = "S";
+
+        System.out.println("Item 1: " + item1.description + ", " + item1.price + ", " + item1.size);
+        System.out.println("Item 2: " + item2.description + ", " + item2.price + ", " + item2.size);
+
+        total = (item1.price + item2.price * 2) * (1 + tax);
+        System.out.println("Total = " + total);
+
     }
-    
+
 }
